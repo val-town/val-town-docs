a668cced3838598a857e0315d832b147d91368e9
Tom MacWright <tom@macwright.com>
2023-11-20 15:01:10 -0500

    Start customizing for VT

---
+++ astro.config.mjs
@@ -0,0 +1,28 @@
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: "Val Town Docs",
+      social: {
+        github: "https://github.com/val-town/val-town-docs",
+        twitter: "https://twitter.com/valdottown",
+      },
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", link: "/guides/example/" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+    }),
+  ],
+});
