9178e1c7cc5226bdf32d0fc9f99c619a1ff88408
Tom MacWright <tom@macwright.com>
2023-11-30 16:20:57 -0500

    Add site parameter for usage with sitemap.xml

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import starlightLinksValidator from "starlight-links-validator";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://docs.val.town/",
   integrations: [
     starlightLinksValidator(),
     starlight({
