39379a1c14afaf75a9a49fe6ef7e7077cddd1e29
Tom MacWright <tom@macwright.com>
2026-04-20 16:12:53 -0400

    Fix biome

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,6 +1,6 @@
 import { defineCollection } from "astro:content";
-import { docsSchema, i18nSchema } from "@astrojs/starlight/schema";
 import { docsLoader, i18nLoader } from "@astrojs/starlight/loaders";
+import { docsSchema, i18nSchema } from "@astrojs/starlight/schema";
 
 export const collections = {
   docs: defineCollection({ loader: docsLoader(), schema: docsSchema() }),
